230da23fa5274be81de67ee8353b9e9fc5c16195
Vu <vco.thien@gmail.com>
2024-05-16 16:45:15 -0700

    ts

--- backend/db/seeders/20231024040757-product-image-seeder.js
+++ backend/db/seeders/20231024040757-product-image-seeder.js
@@ -1,12 +1,11 @@
 'use strict';
 
 /** @type {import('sequelize-cli').Migration} */
-
 let options = {};
 if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA; // define your schema in options object
 }
-options.tableName = 'Product Images'
+options.tableName = 'ProductImages'
 
 module.exports = {
   async up(queryInterface, Sequelize) {
